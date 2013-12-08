1dd4d0d72428854afbc273117082cb92e2c7f583
srikanth kilaru <srikanthkilaru0@gmail.com>
2013-12-08 19:41:45 +0530

    ChatApp Not Working

---
+++ api/models/Messages.js
@@ -0,0 +1,28 @@
+/**
+ * Messages
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+//Messages Model  
+module.exports = {
+    attributes  : {
+        userId: 'INT',
+        username: 'STRING',
+        message: 'STRING'     
+    }
+};
+/*
+module.exports = {
+
+  attributes: {
+  	
+  	 e.g.
+  	nickname: 'string'
+  	
+    
+  }
+
+};*/
---
+++ api/models/Users.js
@@ -0,0 +1,25 @@
+/**
+ * Users
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+//Users Model 
+module.exports = {
+    attributes  : {
+         username: 'STRING',
+         password: 'STRING'     
+    }
+};
+/*module.exports = {
+
+  attributes: {
+  	
+  	/* e.g.
+  	nickname: 'string'
+  	
+    
+  }
+
+};*/
